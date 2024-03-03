93bb566e4f5a29d3a30ab83ab3f39b3ec972aa31
togay-aytemiz <togayaytemiz@gmail.com>
2024-03-03 17:02:05 +0300

    update profile page

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,26 +1,27 @@
 "use client";
 
 import { useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 
 import Profile from "@components/Profile";
 
 const MyProfile = () => {
   const router = useRouter();
   const { data: session } = useSession();
-  const [posts, setPosts] = useState([]);
+
+  const [myPosts, setMyPosts] = useState([]);
 
   useEffect(() => {
     const fetchPosts = async () => {
       const response = await fetch(`/api/users/${session?.user.id}/posts`);
       const data = await response.json();
 
-      setPosts(data);
+      setMyPosts(data);
     };
 
     if (session?.user.id) fetchPosts();
-  }, []);
+  }, [session?.user.id]);
 
   const handleEdit = (post) => {
     router.push(`/update-prompt?id=${post._id}`);
@@ -37,9 +38,9 @@ const MyProfile = () => {
           method: "DELETE",
         });
 
-        const filteredPosts = posts.filter((item) => item._id !== post._id);
+        const filteredPosts = myPosts.filter((item) => item._id !== post._id);
 
-        setPosts(filteredPosts);
+        setMyPosts(filteredPosts);
       } catch (error) {
         console.log(error);
       }
@@ -47,15 +48,14 @@ const MyProfile = () => {
   };
 
   return (
-    <div>
-      <Profile
-        name="My"
-        desc="Welcome to your personalized profile page"
-        data={posts}
-        handleEdit={handleEdit}
-        handleDelete={handleDelete}
-      />
-    </div>
+    <Profile
+      name="My"
+      desc="Welcome to your personalized profile page. Share your exceptional prompts and inspire others with the power of your imagination"
+      data={myPosts}
+      handleEdit={handleEdit}
+      handleDelete={handleDelete}
+    />
   );
 };
+
 export default MyProfile;
